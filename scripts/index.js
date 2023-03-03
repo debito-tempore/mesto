c73af9b1195ae6e1f4972b5eaf1e68b6075d01b6
Elena Starodumova <debito.tempore@yandex.ru>
2023-03-03 18:02:40 +0300

    Вторая попытка спринт5 добавила альт описание

--- scripts/index.js
+++ scripts/index.js
@@ -24,32 +24,32 @@ const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: 'мромр'
+    alt: 'Фотография хребет абишира-ахуба архыз'
   },
   {
     name: 'Челябинская область',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: ''
+    alt: 'Фотография реки среди Уральских гор'
   },
   {
     name: 'Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alt: ''
+    alt: 'Фотография жилого района с высокоэтажками'
   },
   {
     name: 'Камчатка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: ''
+    alt: 'Фотография Камчатских гор'
   },
   {
     name: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alt: ''
+    alt: 'Фотография рельсы уходят за горизонт'
   },
   {
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: ''
+    alt: 'Фотография горного побережья байкала'
   }
 ];
 const createCard = (card) => {
